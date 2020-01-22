d456d251d6299fe2a1aceda5441e90ba4975af46
manish436 <manish436@gmail.com>
2020-01-22 17:12:41 +0530

    Internalization with Spring boot.

--- src/main/java/com/hcl/learn/SpringCloudRestApiApplication.java
+++ src/main/java/com/hcl/learn/SpringCloudRestApiApplication.java
@@ -1,7 +1,12 @@
 package com.hcl.learn;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class SpringCloudRestApiApplication {
@@ -10,4 +15,17 @@ public class SpringCloudRestApiApplication {
 		SpringApplication.run(SpringCloudRestApiApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localResolver = new AcceptHeaderLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localResolver;
+	}
+
+	/*
+	 * @Bean public ResourceBundleMessageSource bundleMessageSource() {
+	 * ResourceBundleMessageSource messageSource = new
+	 * ResourceBundleMessageSource(); messageSource.setBasename("messages"); return
+	 * messageSource; }
+	 */
 }
